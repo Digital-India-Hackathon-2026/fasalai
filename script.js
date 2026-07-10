dd8cbf37446cd897e633d9538e46e2a2fffd56e7
shaikkhasifa623-ar <shaikkasifa623@gmail.com>
2026-07-10 17:39:42 +0530

    Added About page and updated frontend

--- script.js
+++ script.js
@@ -77,3 +77,36 @@ document.addEventListener("DOMContentLoaded", () => {
     showDashboard();
   });
 });
+// =========================
+// Popup Functions
+// =========================
+function openPopup(id) {
+    const popup = document.getElementById(id);
+    popup.style.display = "flex";
+
+    setTimeout(() => {
+        popup.classList.add("show");
+    }, 10);
+}
+
+function closePopup(id) {
+    const popup = document.getElementById(id);
+    popup.classList.remove("show");
+
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 300);
+}
+
+// Close popup when clicking outside
+window.addEventListener("click", function(e) {
+    document.querySelectorAll(".popup").forEach((popup) => {
+        if (e.target === popup) {
+            popup.classList.remove("show");
+
+            setTimeout(() => {
+                popup.style.display = "none";
+            }, 300);
+        }
+    });
+});
